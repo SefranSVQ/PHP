60e75a81ee6f252de5524c00b97f1ae384abcf60
Sefran <sefran2992@gmail.com>
2020-10-22 09:14:57 +0200

    Initial commit

---
+++ 0-holamundo.php
@@ -0,0 +1,14 @@
+<?php 
+   
+    print "Hola sucio humano, bienvenido al increíble mundo de la magia informática de nivel 1";
+
+    for ($i=0 ; $i <= 5 ; $i++){
+        print "<p> $i: Belén, no te duermas </p>";
+    };
+
+    if ($i >= 5){
+        print "Sé contar hasta el $i, que es un número muy grande";
+    }
+
+    
+?>
